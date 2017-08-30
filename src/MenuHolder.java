f8439626ef5294dc95f6b8d511ae23c9618a3744
mnjl1 <d.manzhula@gmail.com>
2017-08-30 22:41:05 +0300

    ver 1.2 class Controller Added ver 1.3 addSkill method, while true added

--- src/MenuHolder.java
+++ src/MenuHolder.java
@@ -9,36 +9,42 @@ public class MenuHolder {
     }
 
     void menu() {
-        System.out.println("Make your choice;");
-        System.out.println("1.Print developers list.");
-        System.out.println("2.Add developer.");
-        System.out.println("3. Delete developer by last name");
-        System.out.println("4. Creat new skill");
-        System.out.println("5. Add new developer' skill");
+        while (true) {
+            System.out.println("Make your choice;");
+            System.out.println("1.Print developers list.");
+            System.out.println("2.Add developer.");
+            System.out.println("3. Delete developer by last name");
+            System.out.println("4. Creat new skill");
+            System.out.println("5. Add new developer' skill");
+            System.out.println("10. Exit.");
 
 
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case (1): {
-                developersDao.getDevelopersList();
-                break;
-            }
-            case (2): {
-                developersDao.createDeveloper();
-                break;
-            }
-            case (3): {
-                developersDao.deleteDeveloper();
-                break;
-            }
-            case (4) : {
-                SkillDAO skillDAO = new SkillDAO();
-                skillDAO.createSkill();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case (1): {
+                    developersDao.getDevelopersList();
+                    break;
+                }
+                case (2): {
+                    developersDao.createDeveloper();
+                    break;
+                }
+                case (3): {
+                    developersDao.deleteDeveloper();
+                    break;
+                }
+                case (4): {
+                    SkillDAO skillDAO = new SkillDAO();
+                    skillDAO.createSkill();
 
-                break;
+                    break;
+                }
+                case (10): {
+                    System.exit(0);
+                }
+                default:
+                    System.out.println("Wrong choice.");
             }
-            default:
-                System.out.println("Wrong choice.");
         }
     }
 }
